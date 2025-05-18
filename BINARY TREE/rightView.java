4245087f13a34ea100a3f90f4deea82b48bc05f9
Ankit Kumar <143324785+ankitkmr150802@users.noreply.github.com>
2025-05-19 01:22:42 +0530

    Update rightView.java

--- BINARY TREE/rightView.java
+++ BINARY TREE/rightView.java
@@ -1,36 +1,25 @@
 class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
+    // Function to return list containing elements of left view of binary tree.
+    ArrayList<Integer> rightView(Node root) {
+        // code here
         ArrayList<Integer> arr = new ArrayList<>();
         if(root == null) return arr;
         
-        Queue<TreeNode> q = new LinkedList<>();
-        //map of (node, vertical level)
-        HashMap<TreeNode, Integer> map = new HashMap<>();
-        
+        Queue<Node> q = new LinkedList<>();
         q.add(root);
-        map.put(root, 0);
         
         while(q.size() > 0){
-            int size = q.size();
-            for(int i = 0; i < size; i++){
-                TreeNode temp = q.remove();
-                int key = map.get(temp);
-                if(i == size-1){
-                    arr.add(temp.val);
+            int len = q.size();
+            for(int i = 0; i < len; i++){
+                Node temp = q.remove();
+                if(i == len-1){
+                    arr.add(temp.data);
                 }
             
-                if(temp.left != null){
-                    q.add(temp.left);
-                    map.put(temp.left, key-1);
-                }
-                if(temp.right != null){
-                    q.add(temp.right);
-                    map.put(temp.right, key+1);
-                }
+                if(temp.left != null)  q.add(temp.left);
+                if(temp.right != null)  q.add(temp.right);
             }
         }
-
         return arr;
-        
     }
 }
