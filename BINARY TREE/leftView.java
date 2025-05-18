5f7e6b10eed6fda6e316cf07b9d2612caf68260b
Ankit Kumar <143324785+ankitkmr150802@users.noreply.github.com>
2025-05-19 01:21:43 +0530

    Update leftView.java

--- BINARY TREE/leftView.java
+++ BINARY TREE/leftView.java
@@ -6,32 +6,20 @@ class Solution {
         if(root == null) return arr;
         
         Queue<Node> q = new LinkedList<>();
-        //map of (node, vertical level)
-        HashMap<Node, Integer> map = new HashMap<>();
-        
         q.add(root);
-        map.put(root, 0);
         
         while(q.size() > 0){
             int size = q.size();
             for(int i = 0; i < size; i++){
                 Node temp = q.remove();
-                int key = map.get(temp);
                 if(i == 0){
                     arr.add(temp.data);
                 }
             
-                if(temp.left != null){
-                    q.add(temp.left);
-                    map.put(temp.left, key-1);
-                }
-                if(temp.right != null){
-                    q.add(temp.right);
-                    map.put(temp.right, key+1);
-                }
+                if(temp.left != null)  q.add(temp.left);
+                if(temp.right != null)  q.add(temp.right);
             }
         }
-
         return arr;
     }
 }
